25bfb2bd952fc55b99ab47e1431f3a3660ca2c47
Wu Han <wuhanstudio@hust.edu.cn>
2019-09-14 21:21:15 +0800

    Fix memory error

--- examples/onnx_parser_ram_example.c
+++ examples/onnx_parser_ram_example.c
@@ -5,7 +5,7 @@ extern unsigned char mnist_onnx[];
 extern unsigned int mnist_onnx_len;
 
 #define THREAD_PRIORITY         8
-#define THREAD_STACK_SIZE       25600
+#define THREAD_STACK_SIZE       1024
 #define THREAD_TIMESLICE        5
 
 static rt_thread_t tid1 = RT_NULL;
@@ -18,17 +18,18 @@ static void onnx_parser_entry(void* parameters)
     // Print Model Info
     if( model != NULL)
     {
+        rt_kprintf("Loaded model\n");
         onnx_model_info(model);
-		// Print Graph Info
-		Onnx__GraphProto* graph = model->graph;
-		if(graph != NULL)
-		{
-			onnx_graph_info_sorted(graph);
-		}
-		
-		// Free Model
-		onnx__model_proto__free_unpacked(model, NULL);
-	}
+        // Print Graph Info
+        Onnx__GraphProto* graph = model->graph;
+        if(graph != NULL)
+        {
+            onnx_graph_info_sorted(graph);
+        }
+        
+        // Free Model
+        onnx__model_proto__free_unpacked(model, NULL);
+    }
     else
     {
         rt_kprintf("Decode model failed\n");
@@ -38,15 +39,18 @@ static void onnx_parser_entry(void* parameters)
 static void onnx_parser_ram_example(int argc, char *argv[])
 {
 
-	tid1 = rt_thread_create("tonnx_ram",
-					onnx_parser_entry, RT_NULL,
-					THREAD_STACK_SIZE,
-					THREAD_PRIORITY, THREAD_TIMESLICE);
+    tid1 = rt_thread_create("tonnx_ram",
+                    onnx_parser_entry, RT_NULL,
+                    THREAD_STACK_SIZE,
+                    THREAD_PRIORITY, THREAD_TIMESLICE);
 
     if (tid1 != RT_NULL)
+    {
         rt_thread_startup(tid1);
-	else
-		rt_kprintf("Start failed\n");
-
+    }
+    else
+    {
+        rt_kprintf("Start failed\n");
+    }
 }
 MSH_CMD_EXPORT(onnx_parser_ram_example, decode onnx model from file);
